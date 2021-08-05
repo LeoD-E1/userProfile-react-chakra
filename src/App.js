c30c0854e9d3f33060c38a0616eb3a8c20fd5c55
leoda <leodangeli11@gmail.com>
2021-08-04 23:35:12 -0300

    error deploy correction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React /* { useEffect, useContext } */ from 'react';
 import ScreenUserProfile from './screens/ScreenUserProfile/ScreenUserProfile';
 // import { UserContext } from './components/User/ContextUser/UserContext';
 
